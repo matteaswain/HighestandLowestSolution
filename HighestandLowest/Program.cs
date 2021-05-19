3492f5658bf82966102ddef2161c9664ca3ee911
Mattea Swain <mmswain@tql.com>
2021-05-19 12:32:08 -0400

    Add project files.

---
+++ HighestandLowest/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace HighestandLowest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nbrs = 
+            {
+               754, 233, 509, 792, 700, 596, 833, 658, 998, 742,
+               187, 754, 308, 914, 489, 867, 717, 586, 929, 467,
+               460, 241, 770, 324, 599, 259, 120, 800, 336, 609,
+               690, 134, 598, 249, 282, 574, 334, 956, 659, 214,
+               435, 643, 809, 874, 906, 620, 328, 369, 426, 561
+            };
+
+
+            var highest = 0;
+            var lowest = 1000;
+
+            foreach (var nbr in nbrs)
+            {
+                if (nbr > highest)
+                {
+                    highest = nbr;
+                }
+                if (nbr < lowest)
+                {
+                    lowest = nbr;
+                }
+            }
+
+            Console.Write($"Lowest is {lowest} and Highest is {highest} !");
+        }
+    }
+}
